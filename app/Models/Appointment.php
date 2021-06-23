d830f76e430c2cb51083ec4b1b38f3a99d3209cf
bl4nc <40602200+bl4nc@users.noreply.github.com>
2021-06-23 14:40:39 -0300

    insert

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -10,6 +10,7 @@ class Appointment extends Model
     // use HasFactory;
 
     protected $table = 'appointment';
+
     protected $fillable = ['name','email','service','phone_number','date','time','your_notes'];
 
 }
