6785ca8cf9f7ce27c54d070aff39f5f385bd9b77
bl4nc <40602200+bl4nc@users.noreply.github.com>
2021-06-23 13:56:29 -0300

    model

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -13,7 +13,18 @@ class AppointmentController extends Controller
     public function insert_appointment(Request $request, Response $response)
     {
         $data = $request->all();
-        print_r($data);
-        $appintment = new Appointment();
+
+        $appointment = new Appointment();
+        $appointment->setName($data['name']);
+        $appointment->setEmail($data['email']);
+        $appointment->setService($data['service']);
+        $appointment->setPhoneNumber($data['phone_number']);
+        $appointment->setDate($data['data']);
+        $appointment->setTime($data['time']);
+        $appointment->setYourNotes($data['your_notes']);
+        $appointment->save();
+
+        return $response()->json(['sucess'=>true,'msg'=>'Appointment send']);
+
     }
 }
