394c7606aab3e4a9669980e02553385fb495e835
bl4nc <40602200+bl4nc@users.noreply.github.com>
2021-06-23 15:20:17 -0300

    finish api

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ use Illuminate\Support\Facades\Route;
 // });
 Route::post('/appointment','App\Http\Controllers\AppointmentController@insert_appointment');
 
-Route::post('/send_contact','App\Http\Controllers\SendContactController@insert_contact');
+Route::post('/send_contact','App\Http\Controllers\SendContactController@send_contact');
