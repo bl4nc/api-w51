569300a8dfc34ee1db3dc855ddfb892a81f95261
bl4nc <40602200+bl4nc@users.noreply.github.com>
2021-06-23 15:05:02 -0300

    insert

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -13,13 +13,17 @@ class AppointmentController extends Controller
     public function insert_appointment(Request $request ,Response $response)
     {
 
+        if (empty($request-> name) || empty($request-> email)){
+            return $response()->json(['sucess'=>false,'msg'=>'Preencha todos os campos!']);
+        }
+
         $appointment = new Appointment;
         $appointment->name = ($request->name ?? '-');
         $appointment->email = ($request->email ?? '-');
         $appointment->service = ($request->service ?? '-');
         $appointment->phone_number = ($request->phone_number ?? '-');
-        $appointment->date = ($request->date ?? '-');
-        $appointment->time = ($request->time ?? '-');
+        $appointment->date = ($request->date);
+        $appointment->time = ($request->time);
         $appointment->your_notes = ($request->your_notes ?? '-');
 
         if ($appointment->save()){
